bb1bf1788c2a9f8a5b500647cacd724786d2c222
Konstantin Zaremski <konstantin.zaremski@gmail.com>
2024-11-03 22:48:14 -0700

    FINAL SUBMISSION

---
+++ pytorch.sh
@@ -0,0 +1,4 @@
+# Command to intall pytoch into the .venv on M1 & later Macs:
+#    Nightly build of pytorch
+pip3 install --pre torch --extra-index-url https://download.pytorch.org/whl/nightly/cpu
+
